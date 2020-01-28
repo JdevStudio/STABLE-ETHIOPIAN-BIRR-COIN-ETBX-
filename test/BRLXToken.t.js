bf38bfcfacfe6534b68b4b4d46d1d693af52f71b
Alexandre <alexandre@55seeds.com>
2020-01-27 21:39:44 -0300

    'refactoring'

--- test/BRLXToken.t.js
+++ test/BRLXToken.t.js
@@ -2,38 +2,36 @@ var BRLXToken = artifacts.require("./BRLXToken.sol");
 
 contract('BRLXToken', function(accounts) {
 
-    var tokenInstance;
-
     it('sets the total supply upon deployment', async () => {
+        
+        // deploy agrrment
         let instance = await BRLXToken.deployed();
-        tokenInstance = instance;
-        let totalSupply = await tokenInstance.totalSupply();
+        let totalSupply = await instance.totalSupply();
 
         assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1000000');
 
         let adminAccount = accounts[0];
-        let adminBalance = await tokenInstance.balanceOf.call(adminAccount);
+        let adminBalance = await instance.balanceOf.call(adminAccount);
         assert.equal(adminBalance.toNumber(), 1000000, 'it allocates initial supply to the admin account');
     })
 
 
     it('Transfers token ownership', async () => {
             let instance = await BRLXToken.deployed();
-            tokenInstance = instance;
 
             let adminAccount = accounts[0];
             let otherAccount = accounts[1];
 
             // Test for transfer larger than balance
             try {
-                await tokenInstance.transfer.call(otherAccount, 999999999999); // tries to send more money that we have
+                await instance.transfer.call(otherAccount, 999999999999); // tries to send more money that we have
             }
             catch(error) {
                 assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
             }
             
             // transfer 250k from admin to other 
-            let receipt = await tokenInstance.transfer(otherAccount, 250000, { from: adminAccount});
+            let receipt = await instance.transfer(otherAccount, 250000, { from: adminAccount});
             
             assert.equal(receipt.logs.length, 1, 'trigger one event');
             assert.equal(receipt.logs[0].event, 'Transfer', 'event should be "Transfer"');
@@ -42,11 +40,11 @@ contract('BRLXToken', function(accounts) {
             assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
 
             // Check admin's balance after transfer
-            let adminBalance = await tokenInstance.balanceOf.call(adminAccount);
+            let adminBalance = await instance.balanceOf.call(adminAccount);
             assert.equal(adminBalance.toNumber(), 750000, 'expected 750k in admin account');
             
             // Check other's balance after transfer
-            let otherBalance = await tokenInstance.balanceOf.call(otherAccount);
+            let otherBalance = await instance.balanceOf.call(otherAccount);
             assert.equal(otherBalance.toNumber(), 250000, 'expected 250k in other account');            
     })
 
