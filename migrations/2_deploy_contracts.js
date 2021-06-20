114a0fb7a410ac6aa38eff5456f3b1845efde9bf
Yoseph Wondafrash <brucegr33n33@gmail.com>
2021-06-20 11:56:58 +0300

    fixed exposed files

--- migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const BRLXToken = artifacts.require("./BRLXToken.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(BRLXToken, 100000000000000, 'Ethiopian Birr Stablecoin', 6, 'ETBS');
-};
